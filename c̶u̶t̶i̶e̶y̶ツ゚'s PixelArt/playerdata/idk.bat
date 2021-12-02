97083e37687e20e80cd2682101ef45b446b40b9d
c̶u̶t̶i̶e̶y̶ツ゚ <93295911+Svxy@users.noreply.github.com>
2021-12-02 01:20:23 -0500

    Add files via upload

---
+++ c̶u̶t̶i̶e̶y̶ツ゚'s PixelArt/playerdata/idk.bat
@@ -0,0 +1,22 @@
+@if (true == false) @end /*!
+@echo off
+mshta "about:
+
+" %*
+goto :EOF */
+
+alert("Hello, world!");
+
+:::::::::::::::::::::::::::::::::::::::::::::::::::
+
+you can call it in a file like this
+
+@echo off
+
+cls
+
+echo enter a password
+
+set /p pass=
+
+call msg
